d8ea1397c57edd477b8813391185737d46120e95
David <daviidccastro@gmail.com>
2022-05-03 23:17:35 -0300

    First Commit

---
+++ bankline-api/src/test/java/com/dio/santander/banklineapi/BanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.banklineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
